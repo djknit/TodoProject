2938095bbe8203b896737c373d8e2fa8b802b8c7
David <djknit@gmail.com>
2021-07-14 15:03:09 -0500

    began user authentication and user controller

---
+++ src/main/java/com/cognixia/jump/service/MyUserDetails.java
@@ -0,0 +1,70 @@
+package com.cognixia.jump.service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cognixia.jump.model.User;
+
+public class MyUserDetails implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+
+	// attributes from User that are needed to do our authorization/authentication
+	private String username;
+	private String password;
+	private List<GrantedAuthority> authorities;
+	
+	public MyUserDetails() {
+		
+	}
+	public MyUserDetails(String username) {
+		this.username = username;
+	}
+	public MyUserDetails(User user) {
+		
+		this.username = user.getUsername();
+		this.password = user.getPassword();
+		this.authorities = Arrays.asList( new SimpleGrantedAuthority( user.getRole().name() ) );
+		
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
